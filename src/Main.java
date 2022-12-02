e883b86a11e514d6167ee722e02c77027451c6dd
Max <m-s-ivanov@yandex.ru>
2022-12-02 17:25:54 +0300

    Временно хочу кое-что проверить

--- src/Main.java
+++ src/Main.java
@@ -75,6 +75,6 @@ public class Main {
         System.out.println("1 - Ввести количество шагов за определённый день;");
         System.out.println("2 - Напечатать статистику за определённый месяц");
         System.out.println("3 - Изменить цель по количеству шагов в день");
-        System.out.println("4 - Выйти из приложения");
+        System.out.println("4 - Выйти из этого приложения");
     }
 }
